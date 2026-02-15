06b00ba178048abfc1e2b4a790ae41278eb90fdf
Tyler B <bremont@usf.edu>
2026-02-15 18:51:38 -0500

    Added inventory dataset

---
+++ cc_4.js
@@ -0,0 +1,8 @@
+// inventory dataset
+const products = [
+  { name: "Wireless Headphones", category: "electronics", price: 79.99, inventory: 12 },
+  { name: "Hoodie", category: "apparel", price: 39.99, inventory: 20 },
+  { name: "Redbull Peach", category: "groceries", price: 2.99, inventory: 60 },
+  { name: "Bleach", category: "household", price: 7.99, inventory: 35 },
+  { name: "Scissors", category: "stationery", price: 5.99, inventory: 100 },
+];
